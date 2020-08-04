3f86737d3c80dbecc72d212501a07cc1290b17c7
ankitlohiya212 <58203694+ankitlohiya212@users.noreply.github.com>
2020-08-04 15:27:18 +0530

    Update twilio.py

--- twilio.py
+++ twilio.py
@@ -2,16 +2,16 @@
 from twilio.rest import Client
 
 account_sid = 'AC2d02634d314e11a6a0629f68d9199006'
-auth_token = '1536d3c8212d741bf4533cc12a68ffc6'
+auth_token = 'your_aut_token'
 client = Client(account_sid, auth_token)
 
 #For Whatsapp
 from_wa_num = 'whatsapp:+14155238886'
-to_wa_num = 'whatsapp:+917507182483'
+to_wa_num = 'whatsapp:your_whatsapp_number'
 
 #For SMS
 from_sms_num = '+18582814263'
-to_sms_num = '+917507182483'
+to_sms_num = '+your_number'
 
 message = client.messages.create(body = 'This is from a bot', from_ = from_wa_num,
                               to = to_wa_num)
